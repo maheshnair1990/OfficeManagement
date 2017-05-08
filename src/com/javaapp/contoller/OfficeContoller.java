cdaf45dd7cfd05c0d2707ff0802e476133a69dbc
Mahesh <mahesh.nair@webaccessglobal.com>
2017-05-08 18:09:13 +0530

    # Added result.jsp # Merged all the view into result.jsp # Changes made in the controller # Text Changes Made in index.jsp

--- src/com/javaapp/contoller/OfficeContoller.java
+++ src/com/javaapp/contoller/OfficeContoller.java
@@ -13,32 +13,38 @@ import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class OfficeContoller {
 
-    @RequestMapping("/welcome")
-    public ModelAndView helloWorld() {
-        return new ModelAndView("welcome", "office", new OfficeBean());
-    }
-
+//    @RequestMapping("/welcome")
+//    public ModelAndView helloWorld() {
+//        return new ModelAndView("welcome", "office", new OfficeBean());
+////         return new ModelAndView("redirect:/enlist");
+//    }
     @RequestMapping(value = "/create", method = RequestMethod.POST)
-    public String create(@ModelAttribute("office") OfficeBean office, ModelMap model) {
+    public String create(@ModelAttribute("office") OfficeBean office, Model model) {
         /*Call to create Office Method*/
         new OfficeModel().createOffice(office);
-        return "edit";
+        List<OfficeBean> list = new OfficeModel().getOffices();
+        model.addAttribute("office", new OfficeBean());
+        model.addAttribute("list", list);
+        return ("result");
     }
 
-    @RequestMapping("/enlist")
-    public ModelAndView enlist() {
+    @RequestMapping("/result")
+    public String enlist(Model model) {
         List<OfficeBean> list = new OfficeModel().getOffices();
-        return new ModelAndView("ShowOffice", "list", list);
+        model.addAttribute("office", new OfficeBean());
+        model.addAttribute("list", list);
+        return ("result");
     }
 
     @RequestMapping(value = "/editoff/{id}")
-    public ModelAndView edit(@PathVariable String id) {
+    public ModelAndView edit(@PathVariable String id, Model model) {
         OfficeBean offBean = new OfficeBean();
         offBean = new OfficeModel().getOffice(id);
         return new ModelAndView("editOffice", "command", offBean);
@@ -47,24 +53,29 @@ public class OfficeContoller {
     @RequestMapping(value = "/editsave", method = RequestMethod.POST)
     public ModelAndView editsave(@ModelAttribute("offBean") OfficeBean off) {
         new OfficeModel().update(off);
-        return new ModelAndView("redirect:/enlist");
+        return new ModelAndView("redirect:/result");
     }
 
     @RequestMapping(value = "/deleteoff/{id}")
     public ModelAndView deleteoff(@PathVariable String id) {
         new OfficeModel().delete(id);
-         return new ModelAndView("redirect:/enlist");
+        return new ModelAndView("redirect:/result");
     }
-    
-     @RequestMapping(value = "/search")
-    public ModelAndView search(@RequestParam String code) {
+
+    @RequestMapping(value = "/search")
+    public String search(@RequestParam String code, Model model) {
         // yourValue contain the value post from the html form
-        String code1=code;
+        String code1 = code;
         System.out.println(code1);
-        OfficeBean offBean=new OfficeModel().getOffice(code);
-        List<OfficeBean> list=new ArrayList<>();
-        list.add(offBean);
-        return new ModelAndView("ShowOffice", "list", list);
+        OfficeBean offBean = new OfficeModel().getOffice(code);
+        List<OfficeBean> list = new ArrayList<>();
+        if( offBean.getCode()!=null){
+         list.add(offBean);
+        }
+       
+        model.addAttribute("office", new OfficeBean());
+        model.addAttribute("list", list);
+        return ("result");
     }
 
 }
